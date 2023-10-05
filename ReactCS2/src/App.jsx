70f012bbc8c5624f3f61ba8ff876cbbcf3b8e758
Olivia <larsonog@hendrix.edu>
2023-10-04 21:16:16 -0500

    Todo list tutorial complete
    I now know everything about React

--- ReactCS2/src/App.jsx
+++ ReactCS2/src/App.jsx
@@ -1,62 +1,52 @@
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewToDoForm"
 import "./style.css"
+import { TodoList } from "./TodoList"
 
-export default function App(){
-  const [newItem, setNewItem]= useState("")
-  //setNewItem("afgae") // causes and infinite loop because of location it reruns over and over 
-  const [todos, setTodos] = useState([])
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
 
-  function handleSumbit(e){
-      e.preventDefault()
+    return JSON.parse(localValue)
+  })
 
-      setTodos((currentTodos) => {
-        return(
-          [... currentTodos, {id:crypto.randomUUID(), title: newItem, completed:false},
-          ]
-        )
-      })
-      setNewItem("") // c;ears out input box
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
   }
 
-  function toggleToDo(id, completed){
-    setTodos(currentTodos=> {
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
       return currentTodos.map(todo => {
-        if (todo.id === id){
-          // todo.complete = completed YOU CANT DO THIS bc its immutable
-          return {...todo, completed} // creates a brand new state object
-          return todo
+        if (todo.id === id) {
+          return { ...todo, completed }
         }
+
+        return todo
       })
     })
   }
-  return( 
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
     <>
-    <form onSubmit= {handleSubmit} className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item"> New Item</label>
-        <input 
-        value = {newItem} 
-        onChange = {e => setNewitem(e.target.value)} // with no on change the input will always be empty bc we declared it as such with useState
-        type="text" 
-        id="item" />
-      </div>
-      <button className="btn"> Add</button>
-    </form>
-  <ht className="header"> ToDo </ht>
-  <ul className="list">
-    {todos.map (todo => { // runs it through javascript code
-      return (   //don't use index bc it gets tricky when you strat deleting elements
-        <li key={todo.id}> 
-          <label>
-            <input type="checkbox" checked= {todo.completed}
-            onChange= {e => toggleToDo(todo.id, e.target.check)}/>
-            {todo.title}
-          </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-      )
-    })} 
-  </ul>
-  </>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
   )
 }
